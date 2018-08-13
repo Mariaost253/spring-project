d20796835367821399d1b01d9b1f482812775227
mariaost253 <mariaost253@gmail.com>
2018-08-13 05:19:56 +0300

    final commit

--- src/main/java/spring/project/controller/ItemRepositary.java
+++ src/main/java/spring/project/controller/ItemRepositary.java
@@ -1,8 +1,7 @@
 package spring.project.controller;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 @Repository
--- src/main/java/spring/project/controller/ItemService.java
+++ src/main/java/spring/project/controller/ItemService.java
@@ -6,9 +6,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
-import org.springframework.context.annotation.Bean;
+
 
 @Service
 
